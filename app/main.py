781642b59e0731994c76d892873ac161eea73c14
kovalroma <koval.roma@gmail.com>
2024-06-05 16:59:22 +0200

    dockerize Flask app

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Get the current date and time
+    current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Get the IP address of the server
+    server_ip = request.host.split(':')[0]
+
+    return {
+        'current_date': current_date,
+        'server_ip': server_ip
+    }
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
