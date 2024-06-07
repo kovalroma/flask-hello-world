cb4837cd0cf66a56c92e4f10049c2d80f5b2a3ae
kovalroma <koval.roma@gmail.com>
2024-06-07 19:42:55 +0200

    Add server IP

--- hello-world-app/app.py
+++ hello-world-app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from datetime import datetime
-import socket
+import socket, subprocess
 
 app = Flask(__name__)
 
@@ -10,9 +10,10 @@ def index():
     current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     # Get the IP address of the server
-    server_ip = request.host.split(':')[0]
+    server_hostname = request.host.split(':')[0]
+    server_ip = subprocess.check_output(["hostname", "-I"], text=True)
 
-    return render_template('index.html', current_date=current_date, server_ip=server_ip)
+    return render_template('index.html', current_date=current_date, server_hostname=server_hostname, server_ip=server_ip)
 
 
 if __name__ == '__main__':
